b07bb0a3e8dd7c6be63a833245eecabdcb7e63a8
Crístian Deives <cristiandeives@gmail.com>
2020-04-05 22:04:09 -0300

    Create anonymous user on first launch
    Signed-off-by: Crístian Deives <cristiandeives@gmail.com>

--- app/src/main/java/com/gmail/cristiandeives/bogafit/ListPhysictivityViewModel.kt
+++ app/src/main/java/com/gmail/cristiandeives/bogafit/ListPhysictivityViewModel.kt
@@ -3,26 +3,58 @@ package com.gmail.cristiandeives.bogafit
 import android.util.Log
 import androidx.annotation.MainThread
 import androidx.annotation.UiThread
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.gmail.cristiandeives.bogafit.data.FirestoreRepository
 import com.gmail.cristiandeives.bogafit.data.Physictivity
 import com.gmail.cristiandeives.bogafit.data.toPhysictivity
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.DocumentSnapshot
 
 @MainThread
 class ListPhysictivityViewModel : ViewModel() {
     private val repo = FirestoreRepository.getInstance()
+    private val auth = FirebaseAuth.getInstance()
 
     private val _listPhysictivitiesStatus = MutableLiveData<Resource<List<Physictivity>>>()
     val listPhysictivityStatus = _listPhysictivitiesStatus
 
+    private val _currentUserStatus = MutableLiveData<Resource<*>>().apply {
+        value = auth.currentUser?.let { Resource.Success<Any>() }
+    }
+    val currentUserStatus: LiveData<Resource<*>> = _currentUserStatus
+
     init {
-        listenToPhysictivities()
+        if (_currentUserStatus.value == null) {
+            signInAnonymous()
+        }
     }
 
     @UiThread
-    private fun listenToPhysictivities() {
+    private fun signInAnonymous() {
+        Log.d(TAG, "signing in with an anonymous account...")
+        _currentUserStatus.value = Resource.Loading<Any>()
+
+        auth.signInAnonymously().addOnSuccessListener { result ->
+            val uid = result.user?.uid.orEmpty()
+            Log.d(TAG, "user is now signed in (UID=$uid)")
+            _currentUserStatus.value = Resource.Success<Any>()
+        }.addOnFailureListener {  ex ->
+            Log.w(TAG, "anonymous sign in failed: ${ex.message}", ex)
+            _currentUserStatus.value = Resource.Error<Any>(ex)
+        }.addOnCanceledListener {
+            Log.d(TAG, "anonymous sign in canceled")
+            _currentUserStatus.value = Resource.Canceled<Any>()
+        }
+    }
+
+    @UiThread
+    fun listenToPhysictivities() {
+        if (_listPhysictivitiesStatus.value != null) {
+            return
+        }
+
         _listPhysictivitiesStatus.value = Resource.Loading()
 
         repo.getPhysictivitiesQuery().addSnapshotListener { snap, ex ->
--- app/src/main/java/com/gmail/cristiandeives/bogafit/data/FirestoreRepository.kt
+++ app/src/main/java/com/gmail/cristiandeives/bogafit/data/FirestoreRepository.kt
@@ -2,6 +2,7 @@ package com.gmail.cristiandeives.bogafit.data
 
 import android.util.Log
 import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
@@ -10,9 +11,13 @@ import java.time.LocalDate
 
 class FirestoreRepository {
     private val db = FirebaseFirestore.getInstance()
+    private val auth = FirebaseAuth.getInstance()
 
     fun addPhysictivity(date: LocalDate, type: Physictivity.Type): Task<DocumentReference> {
+        val uid = auth.currentUser?.uid ?: throw IllegalStateException("there is no signed in user")
+
         val data = mapOf(
+            PHYSICTIVITY_UID to uid,
             PHYSICTIVITY_FIELD_DATE to date.toFirestoreTimestamp(),
             PHYSICTIVITY_FIELD_TYPE to type,
             PHYSICTIVITY_FIELD_UPDATED_AT to FieldValue.serverTimestamp()
@@ -30,12 +35,15 @@ class FirestoreRepository {
         )
 
         Log.d(TAG, "editing document [id=${id}, $data] in collection $PHYSICTIVITY_COLLECTION...")
-        return db.collection(PHYSICTIVITY_COLLECTION).document(id).set(data)
+        return db.collection(PHYSICTIVITY_COLLECTION).document(id).update(data)
     }
 
     fun getPhysictivitiesQuery(): Query {
-        Log.d(TAG, "querying documents from collection $PHYSICTIVITY_COLLECTION")
+        val uid = auth.currentUser?.uid ?: throw IllegalStateException("there is no signed in user")
+
+        Log.d(TAG, "querying documents from collection $PHYSICTIVITY_COLLECTION with UID=$uid")
         return db.collection(PHYSICTIVITY_COLLECTION)
+            .whereEqualTo(PHYSICTIVITY_UID, uid)
             .orderBy(PHYSICTIVITY_FIELD_DATE, Query.Direction.DESCENDING)
     }
 
@@ -46,6 +54,7 @@ class FirestoreRepository {
 
     companion object {
         private const val PHYSICTIVITY_COLLECTION = "physictivity"
+        const val PHYSICTIVITY_UID = "uid"
         const val PHYSICTIVITY_FIELD_DATE = "date"
         const val PHYSICTIVITY_FIELD_TYPE = "type"
         const val PHYSICTIVITY_FIELD_UPDATED_AT = "updatedAt"
