72e0d6d3e9977e690ad5b38443b67c999c2d6b3f
Crístian Deives <cristiandeives@gmail.com>
2020-04-17 18:37:50 -0300

    Use Toolbar explicitly in Activities
    Signed-off-by: Crístian Deives <cristiandeives@gmail.com>

--- app/src/main/java/com/gmail/cristiandeives/bogafit/MainActivity.kt
+++ app/src/main/java/com/gmail/cristiandeives/bogafit/MainActivity.kt
@@ -4,17 +4,22 @@ import android.os.Bundle
 import android.util.Log
 import androidx.annotation.MainThread
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
+import com.gmail.cristiandeives.bogafit.databinding.ActivityMainBinding
 
 @MainThread
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
+class MainActivity : AppCompatActivity() {
     private val navController by lazy { findNavController(R.id.nav_host_fragment) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.v(TAG, "> onCreate(...)")
         super.onCreate(savedInstanceState)
 
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        setSupportActionBar(binding.actionBar)
         setupActionBarWithNavController(navController)
 
         Log.v(TAG, "< onCreate(...)")
